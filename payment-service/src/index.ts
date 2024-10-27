ef3da21256596f776c3ffec685e4b52d5d414999
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-10-28 00:06:40 +0700

    add credentials

--- payment-service/src/index.ts
+++ payment-service/src/index.ts
@@ -16,6 +16,7 @@ app.use(
     origin: "*",
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
   })
 );
 app.use(bodyParser.urlencoded({ extended: false }));
