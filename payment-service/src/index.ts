4aa69b165f091de896201be3d45b654ca934dc31
nguynthuhigh <22dh114648@st.huflit.edu.vn>
2024-11-23 21:38:07 +0700

    fix: cors

--- payment-service/src/index.ts
+++ payment-service/src/index.ts
@@ -11,14 +11,7 @@ import cors from "cors";
 connectMongoDB();
 connectRedis();
 // app.use("/api", proxyMiddleware);
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  res.header("Access-Control-Allow-Origin", origin || "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api", router);
