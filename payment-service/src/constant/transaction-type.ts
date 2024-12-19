1ea26dd7ae873bfcff9b0bb1e1e580d754c4ec1e
nguynthuhigh <22dh114648@st.huflit.edu.vn>
2024-12-19 23:43:44 +0700

    Deploy website - based on 82c53dd510dfe49ff989aae1ec8cfa56df157218

--- payment-service/src/constant/transaction-type.ts
+++
@@ -1,8 +0,0 @@
-export const TRANSACTION_TYPE = {
-  TRANSFER: "transfer",
-  PAYMENT: "payment",
-  DEPOSIT: "deposit",
-  WITHDRAW: "withdraw",
-  WITHDRAW_PARTNER: "withdraw-partner",
-  REFUND: "refund",
-};
